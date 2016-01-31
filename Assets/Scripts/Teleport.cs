e8f0b35793b7143b8c25ba9449c0ae1e6292c056
Paolla Catherine <paollacath@gmail.com>
2016-01-31 16:39:44 -0200

    Adicionando música de proximidade ao portal e play quando há colisão com o Player

--- Assets/Scripts/Teleport.cs
+++ Assets/Scripts/Teleport.cs
@@ -1,14 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Teleport : MonoBehaviour {
+public class Teleport : MonoBehaviour
+{
+    private AudioSource _audio;
     public Transform tTeleport;
     //public Transform tChar;
 
-    void OnTriggerEnter(Collider other) {
-        if (other.tag == "Player") {
-            other.gameObject.transform.position = tTeleport.position;        
+    void Awake() {
+        this._audio = this.GetComponent<AudioSource>();
+    }
+   
+
+    void OnTriggerEnter(Collider other)
+    {
+        this._audio.Play();
+        if (other.tag == "Player"){
+            other.gameObject.transform.position = tTeleport.position;
         }
     }
 }
-
