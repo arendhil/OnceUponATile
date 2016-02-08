90edc27f187c0001540a689ee0d61f62b34f6d3e
Marcelo de Freitas Rigon <arendhil@gmail.com>
2016-02-08 18:52:38 -0200

    Head Controller funciona, precisa trabalhar no algoritmo que escolhe o item que ele está olhando.

---
+++ Assets/Scripts/HeadController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeadController : MonoBehaviour {
+    Animator _animator;
+    public GameObject pointOfInterest;
+
+    void Start () {
+        this._animator = this.GetComponent<Animator>();
+    }
+
+    // control the ik on OnAnimatorIK.
+    void OnAnimatorIK (int layerIndex) {
+        if (pointOfInterest != null) {
+            this._animator.SetLookAtPosition(pointOfInterest.transform.position);
+            this._animator.SetLookAtWeight(0.8f);
+        }
+    }
+}
